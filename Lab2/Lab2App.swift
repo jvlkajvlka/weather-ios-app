c8c20045c602db387590b057b809141b7fddb749
qvgabi <gabimatuszewska3@gmail.com>
2021-05-05 12:46:52 +0200

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Gabi Matuszewska on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
